7815fb35225c67217a4017f38cac21ef93369fc3
Shashank <shashank.chakravarthy@gmail.com>
2016-10-29 04:49:26 +0530

    Added comment

--- download_files.sh
+++ download_files.sh
@@ -18,7 +18,8 @@ for line in `cat filenames_only.txt`
 do
 	#the field separator for each line is given as the fourth field. Might be wrong for many other"
 	#filename=`echo $line | tr '\n' ' ' | cut -d':' -f4`
-	#Grep is more effective and flexible to extract path and file name with ext. Change regex as necessary
+	#Grep is more effective and flexible to extract path and file name with ext. Change regex as necessary.
+	#To try powerful regex, look at option P
 	filename=`echo $line | tr '\n' ' ' | grep -oie 'text[a-zA-Z0-9.\/\(\)\ -]*.pdf'`
 
 	#the first sed replacement is for ctrl-m character which is "return". 
