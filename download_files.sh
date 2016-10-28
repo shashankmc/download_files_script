f94f081c2db81ebf4c17e7ca9c4246bca9714b58
Shashank <shashank.chakravarthy@gmail.com>
2016-10-29 04:36:26 +0530

    Changed variable names and comments

--- download_files.sh
+++ download_files.sh
@@ -4,12 +4,12 @@
 mkdir Files_Download
 cd Files_Download
 
-#base url from which files can be downloaded"
+#base url from which files can be downloaded, change as necessary
 url="http://ktbs.kar.nic.in/New/"
 
 #contains link to the file which contains all the file names available to download"
-download_file_list="http://ktbs.kar.nic.in/New/jsonfile/textbooks.txt"
-`wget $download_file_list`
+file_list="http://ktbs.kar.nic.in/New/jsonfile/textbooks.txt"
+`wget $file_list`
 `cat textbooks.txt | grep "pdf" > filenames_only.txt`
 
 #set Field separator to identify when next line occurs"
@@ -18,7 +18,7 @@ for line in `cat filenames_only.txt`
 do
 	#the field separator for each line is given as the fourth field. Might be wrong for many other"
 	#filename=`echo $line | tr '\n' ' ' | cut -d':' -f4`
-	#Grep is more effective and flexible to extract path and file name with ext.
+	#Grep is more effective and flexible to extract path and file name with ext. Change regex as necessary
 	filename=`echo $line | tr '\n' ' ' | grep -oie 'text[a-zA-Z0-9.\/\(\)\ -]*.pdf'` 
 	clean_filename=`echo $filename | sed 's///g'|sed 's/$//g' | sed 's/,//g' | sed 's/\}//g' | sed 's/"//g' | sed 's/\ //g'`
 	`wget $url$clean_filename`
